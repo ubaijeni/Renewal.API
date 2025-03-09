ffe2ea84af4ab7a954f580c3737c05d245f3ac77
Nathan <42325916+nathlemma@users.noreply.github.com>
2025-03-09 11:37:32 -0400

    nathan initial commit

---
+++ Renewal.Service/BusinessLogic/PODetails.cs
@@ -0,0 +1,34 @@
+namespace Renewal.Service.BusinessLogic;
+
+public class PODetails
+{
+    // . TABLE [dbo].[PODetails](
+    // [POID] [int] IDENTITY(1,1) NOT NULL,
+    // [POTypeID] [int] NULL,
+    // [PONumber] [nvarchar](max) NULL,
+    // [POvalue] [decimal](18, 2) NULL,
+    // [POStatusID] [int] NULL,
+    // [ClientNameID] [int] NULL,
+    // [EngagementModelID] [int] NULL,
+    // [ResourceprojectName] [nvarchar](max) NULL,
+    // [LocationID] [int] NULL,
+    // [Accountable] [nvarchar](max) NULL,
+    // [CurrencyID] [int] NULL,
+    // [JoiningDate] [nvarchar](max) NULL,
+    // [StartDate] [nvarchar](max) NULL,
+    // [EndDate] [nvarchar](max) NULL,
+    // [ContractPeriod] [nvarchar](200) NULL,
+    // [RenewalDate] [nvarchar](max) NULL,
+    // [RemindertoAlert] [nvarchar](max) NULL,
+    // [Remarks] [nvarchar](max) NULL,
+    // [POFileName] [nvarchar](max) NULL,
+    // [Isactive] [int] NULL,
+    // [Suspend] [int] NULL,
+    // [Createddatetime] [datetime] NULL,
+    // [CreatedBy] [int] NULL,
+    // [Updateddatetime] [datetime] NULL,
+    // [UpdatedBy] [int] NULL,
+    // [DMPOC] [nvarchar](255) NULL,
+    // [FMPOC] [nvarchar](255) NULL,
+    // [ProposalPath] [nvarchar](255) NULL,
+}
