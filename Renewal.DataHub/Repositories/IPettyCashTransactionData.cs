6a86aed4c0339a743f7b9e3d184b452145814b9c
Imadh <mohamedimadhu432@gmail.com>
2025-03-14 11:34:00 +0530

    DataHubLayer

---
+++ Renewal.DataHub/Repositories/IPettyCashTransactionData.cs
@@ -0,0 +1,13 @@
+﻿using Renewal.DataHub.Models.Domain;
+
+namespace Renewal.DataHub.Models.Repository
+{
+    public interface IPettyCashTransactionData
+    {
+        Task<IEnumerable<PettyCashTransaction>> GetAllTransactionsAsync(Guid? branchId, DateTime? startDate, DateTime? endDate);
+        Task<PettyCashTransaction> GetTransactionByIdAsync(Guid transactionId);
+        Task AddTransactionAsync(PettyCashTransaction transaction);
+        Task UpdateTransactionAsync(PettyCashTransaction transaction);
+        Task DeleteTransactionAsync(Guid transactionId);
+    }
+}
